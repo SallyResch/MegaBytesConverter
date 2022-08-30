fd32b2f8e1f09f732cff06e1a57806217ce0e190
Sally Resch <sallyresch27@gmail.com>
2022-08-30 11:38:35 +0200

    Initial commit

---
+++ src/MegaBytesConverter.java
@@ -0,0 +1,10 @@
+public class MegaBytesConverter {
+    public static void printMegaBytesAndKiloBytes (int kiloBytes){
+        if(kiloBytes < 0){
+            System.out.println("Invalid Value");
+        } else (kiloBytes > 0){
+            
+            System.out.println();
+        }
+    }
+}
